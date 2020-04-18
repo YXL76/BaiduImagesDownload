8578f7da814295b6bfa76a389551219fa153cf24
YXL <chenxin.lan.76@gmail.com>
2020-04-18 15:11:47 +0800

    ✨feat(Crawler): Async use Asynchronous HTTP Client to download images

--- BaiduImagesDownload/crawler.py
+++ BaiduImagesDownload/crawler.py
@@ -1,14 +1,16 @@
+from aiohttp import ClientError as async_ClientError, ClientSession, ClientTimeout
+from asyncio import TimeoutError as async_TimeoutError, ensure_future, gather, get_event_loop
 from json import loads
 from mimetypes import guess_extension
-from os import mkdir
-from os.path import exists, join
-from requests.exceptions import ProxyError
+from os import listdir, mkdir
+from os.path import exists, join, splitext
 from re import search
 from requests import get
+from shutil import copyfile
 from string import Template
+from tempfile import TemporaryDirectory
 from time import sleep
 from tqdm import tqdm
-from urllib.parse import urlsplit
 
 
 class Crawler:
@@ -18,12 +20,12 @@ class Crawler:
     }
     __BASE_URL = 'https://image.baidu.com/search/acjson'
     __PAGE_NUM = 50
+    __CONCURRENT_NUM = 100
     __OBJURL_TABLE = {'_z2C$q': ':', '_z&e3B': '.', 'AzdH3F': '/'}
     __OBJURL_TRANS = str.maketrans('0123456789abcdefghijklmnopqrstuvw', '7dgjmoru140852vsnkheb963wtqplifca')
     __FILENAME_TEMPLATE = Template('${name}${ext}')
 
-    def __init__(self, interval: float = 0.01):
-        self.__interval = interval
+    def __init__(self):
         self.__params = {
             'face': 0,
             'ie': 'utf-8',
@@ -35,11 +37,6 @@ class Crawler:
         }
         self.__urls = []
         print('----INFO----初始化成功')
-
-    @staticmethod
-    def __mkdir_download(folder: str) -> None:
-        if not exists(folder):
-            mkdir(folder)
 
     def __decode_objurl(self, url: str) -> str:
         for key, value in self.__OBJURL_TABLE.items():
@@ -60,16 +57,6 @@ class Crawler:
         urls['obj_url'].append(img['thumbURL'])
         urls['from_url'].append('')
         return urls
-
-    @staticmethod
-    def __check_type(mime_type: str, rule: tuple):
-        allow = False
-        ext = guess_extension(mime_type)
-        if ext in ('.jpe', '.jpeg'):
-            ext = '.jpg'
-        if ext in rule:
-            allow = True
-        return allow, ext
 
     def clear(self) -> None:
         self.__params['pn'] = 0
@@ -118,67 +105,95 @@ class Crawler:
                 else:
                     pbar.update(length)
 
-                sleep(self.__interval)
-
         print('----INFO----获取图片url成功')
-        if len(self.__urls) > num:
-            self.__urls = self.__urls[0:num]
         sleep(0.4)
         return True
 
     def download_images(self, rule: tuple, num: int = None, folder: str = 'download') -> bool:
-        self.__mkdir_download(folder)
-        if num is None:
-            num = len(self.__urls)
-        elif num > len(self.__urls):
-            num = len(self.__urls)
-            print('----WARM----图片数量不足, 只有', num, '张')
+
+        async def __check_type(mime_type: str) -> (bool, str):
+            allow = False
+            ext = guess_extension(mime_type)
+            if ext in ('.jpe', '.jpeg'):
+                ext = '.jpg'
+            if ext in rule:
+                allow = True
+            return allow, ext
+
+        async def fetch(session, obj_url: str, from_url: str, idx: int) -> bool:
+            headers.update({'Referer': from_url})
+            try:
+                async with session.get(obj_url, headers=headers, allow_redirects=False) as res:
+                    if res.status == 200:
+                        allow, ext = await __check_type(res.headers['content-type'].partition(';')[0].strip())
+                        if allow is False:
+                            return False
+                        with open(join(tmpdirname, self.__FILENAME_TEMPLATE.substitute(name=idx, ext=ext)),
+                                  mode='wb') as f:
+                            while True:
+                                chunk = await res.content.read(10)
+                                if not chunk:
+                                    break
+                                f.write(chunk)
+            except (async_ClientError, async_TimeoutError):
+                return False
+            return True
+
+        async def fetch_all(url: dict, idx: int) -> None:
+            nonlocal success
+            allow = False
+            for j in range(len(url['obj_url'])):
+                async with ClientSession(timeout=ClientTimeout(total=60)) as session:
+                    allow = await fetch(session, url['obj_url'][j], url['from_url'][j], idx + 1)
+                    if allow is True:
+                        break
+            if allow is False:
+                failed.append(idx + 1)
+            elif success < num:
+                success += 1
+                pbar.update(1)
+
+        if not exists(folder):
+            mkdir(folder)
+
+        success = 0
         failed = []
         headers = self.__HEADERS.copy()
-        for i in tqdm(range(num), desc='下载图片'):
-            res = None
-            ext = None
-            allow = False
+        pbar = tqdm(total=num, desc='下载图片')
 
-            for j in range(len(self.__urls[i]['obj_url'])):
-                obj_url = self.__urls[i]['obj_url'][j]
-                from_url = self.__urls[i]['from_url'][j]
-                referer = {
-                    'Referer': from_url
-                }
-                headers.update(referer)
-                try:
-                    res = get(obj_url, headers=headers)
-                    if res.status_code == 200:
-                        allow, ext = self.__check_type(res.headers['content-type'].partition(';')[0].strip(), rule)
-                        if allow is False:
-                            continue
-                        else:
-                            break
-                except ProxyError:
-                    pass
+        if num is None or num > len(self.__urls):
+            num = len(self.__urls)
 
-            if res is None or allow is False:
-                failed.append(str(i + 1))
-                continue
+        with TemporaryDirectory() as tmpdirname:
+            for i in range(0, len(self.__urls), self.__CONCURRENT_NUM):
+                loop = get_event_loop()
+                tasks = [ensure_future(fetch_all(url, i + idx))
+                         for idx, url in enumerate(self.__urls[i:i + self.__CONCURRENT_NUM])]
+                tasks = gather(*tasks)
+                loop.run_until_complete(tasks)
 
-            with open(join(folder, self.__FILENAME_TEMPLATE.substitute(name=(i + 1), ext=ext)), 'wb') as f:
-                f.write(res.content)
+            num_length = len(str(num))
+            for idx, filename in enumerate(listdir(tmpdirname)):
+                if idx >= num:
+                    break
+                copyfile(join(tmpdirname, filename),
+                         join(folder, str(idx + 1).zfill(num_length) + splitext(filename)[1]))
 
-            sleep(self.__interval)
-
+        pbar.update(num - success)
+        pbar.close()
         print('----INFO----图片下载结束')
         if failed:
-            print('----ERROR---第', ', '.join(failed), '张图片下载失败')
+            print('----ERROR---', len(failed), '张图片下载失败')
             return False
         return True
 
-    def start(self, word: str, num: int, rule: tuple = None):
+    def start(self, word: str, num: int, rule: tuple = None) -> None:
         self.clear()
         if rule is None:
             rule = ('.png', '.jpg')
         if self.get_images_url(word, int(num * 1.5)):
             self.download_images(rule, num)
+        self.clear()
 
 
 if __name__ == '__main__':
