4b42950cdc92f88d8f2dc67caca7d90dc9fa4d2d
YXL <chenxin.lan.76@gmail.com>
2020-05-20 02:51:26 +0000

    🦄refactor(crawler): make all metheds static

--- BaiduImagesDownload/crawler.py
+++ BaiduImagesDownload/crawler.py
@@ -1,24 +1,25 @@
-from aiohttp import ClientError as async_ClientError, ClientSession, ClientTimeout
+import logging
 from asyncio import TimeoutError as async_TimeoutError, ensure_future, gather, get_event_loop
 from json import loads
-import logging
 from mimetypes import guess_extension
 from os import listdir, mkdir
-from os.path import exists, join, splitext
+from os.path import join, splitext
 from re import search
 from shutil import copyfile
 from string import Template
 from tempfile import TemporaryDirectory
-from tqdm import tqdm
 from urllib.parse import urlencode
 from urllib.request import urlopen
+
+from aiohttp import ClientError as async_ClientError, ClientSession, ClientTimeout
+from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(logging.Formatter(
-    '%(asctime)s [%(levelname)s] %(message)s'))
+    '[%(asctime)s] [%(levelname)s] %(message)s'))
 logger.addHandler(stream_handler)
 
 
@@ -35,19 +36,15 @@ class Crawler:
     __OBJURL_TRANS = str.maketrans(
         '0123456789abcdefghijklmnopqrstuvw', '7dgjmoru140852vsnkheb963wtqplifca')
     __FILENAME_TEMPLATE = Template('${name}${ext}')
-
-    def __init__(self, timeout: float = __CONCURRENT_TIMEOUT):
-        self.__params = {
-            'face': 0,
-            'ie': 'utf-8',
-            'ipn': 'rj',
-            'oe': 'utf-8',
-            'pn': 0,
-            'rn': Crawler.__PAGE_NUM,
-            'tn': 'resultjson_com',
-        }
-        self.__timeout = timeout
-        logger.info('初始化成功')
+    __PARAMS = {
+        'face': 0,
+        'ie': 'utf-8',
+        'ipn': 'rj',
+        'oe': 'utf-8',
+        'pn': 0,
+        'rn': __PAGE_NUM,
+        'tn': 'resultjson_com',
+    }
 
     @staticmethod
     def decode_objurl(url: str) -> str:
@@ -78,16 +75,17 @@ class Crawler:
 
         return url
 
-    def get_images_url(self, word: str, num: int) -> (bool, bool, list):
+    @staticmethod
+    def get_images_url(word: str, num: int, timeout: int = __CONCURRENT_TIMEOUT) -> (bool, bool, list):
 
         async def __fetch(pn: int) -> None:
             nonlocal net
-            params = self.__params.copy()
-            params['pn'] = pn
+            par = params.copy()
+            par['pn'] = pn
 
-            async with ClientSession(timeout=ClientTimeout(total=self.__timeout)) as session:
+            async with ClientSession(timeout=ClientTimeout(total=timeout)) as session:
                 try:
-                    async with session.get(Crawler.__BASE_URL, params=params, headers=Crawler.__HEADERS) as res:
+                    async with session.get(Crawler.__BASE_URL, params=par, headers=Crawler.__HEADERS) as res:
                         if res.status == 200:
                             text = await res.text()
                             logger.debug(text)
@@ -106,13 +104,13 @@ class Crawler:
         eng = True
         urls = []
 
-        self.__params['pn'] = 0
-        self.__params['queryWord'] = word
-        self.__params['word'] = word
+        params = Crawler.__PARAMS.copy()
+        params['queryWord'] = word
+        params['word'] = word
 
         logger.info('开始获取图片url')
 
-        with urlopen((Crawler.__BASE_URL + '?%s') % urlencode(self.__params)) as r:
+        with urlopen((Crawler.__BASE_URL + '?%s') % urlencode(params)) as r:
             if r.status != 200:
                 net = False
             else:
@@ -153,8 +151,9 @@ class Crawler:
             allow = True
         return allow, ext
 
-    def download_images(self, urls: list,
-                        rule: tuple = ('.png', '.jpg'), path: str = 'download') -> (int, int):
+    @staticmethod
+    def download_images(urls: list, rule: tuple = ('.png', '.jpg'),
+                        path: str = 'download', timeout: int = __CONCURRENT_TIMEOUT) -> (int, int):
 
         async def __fetch(session, obj_url: str, from_url: str, idx: int) -> bool:
             headers = Crawler.__HEADERS.copy()
@@ -162,7 +161,8 @@ class Crawler:
             try:
                 async with session.get(obj_url, headers=headers, allow_redirects=False) as res:
                     if res.status == 200 and 'content-type' in res.headers:
-                        allow, ext = await Crawler.__check_type(res.headers['content-type'].partition(';')[0].strip(), rule)
+                        allow, ext = await Crawler.__check_type(res.headers['content-type'].partition(';')[0].strip(),
+                                                                rule)
 
                         if allow is False:
                             return False
@@ -181,7 +181,7 @@ class Crawler:
 
         async def __fetch_all(url: dict, idx: int) -> None:
             for j in range(len(url['obj_url'])):
-                async with ClientSession(timeout=ClientTimeout(total=self.__timeout)) as session:
+                async with ClientSession(timeout=ClientTimeout(total=timeout)) as session:
                     allow = await __fetch(session, url['obj_url'][j], url['from_url'][j], idx + 1)
                     if allow is True:
                         pbar.update(1)
@@ -226,6 +226,5 @@ class Crawler:
 
 if __name__ == '__main__':
     logger.setLevel(logging.DEBUG)
-    crawler = Crawler()
-    net, num, urls = crawler.get_images_url('二次元', 20)
-    crawler.download_images(urls)
+    net_, num_, urls_ = Crawler.get_images_url('二次元', 20)
+    Crawler.download_images(urls_)
